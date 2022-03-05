7d61983c42c4bbb9ff1716874d8c945565e9a3d3
Andrew Le <s3820098@rmit.edu.vn>
2022-03-05 12:40:42 +0700

    7. Simplify images imports

---
+++ react-application/src/assets/images.js
@@ -0,0 +1,23 @@
+import Product1 from './images/image-product-1.jpg';
+import Product1Thumbnail from './images/image-product-1-thumbnail.jpg';
+import Product2 from './images/image-product-2.jpg';
+import Product2Thumbnail from './images/image-product-2-thumbnail.jpg';
+import Product3 from './images/image-product-3.jpg';
+import Product3Thumbnail from './images/image-product-3-thumbnail.jpg';
+import Product4 from './images/image-product-4.jpg';
+import Product4Thumbnail from './images/image-product-4-thumbnail.jpg';
+import HeadIcon from './images/favicon-32x32.png';
+
+const images = {
+  Product1,
+  Product2,
+  Product3,
+  Product4,
+  Product1Thumbnail,
+  Product2Thumbnail,
+  Product3Thumbnail,
+  Product4Thumbnail,
+  HeadIcon,
+};
+
+export default images;
